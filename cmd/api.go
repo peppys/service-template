e33c21ac9f9dcc964dce92716a788a854d260e32
Peppy Sisay <psisay@netflix.com>
2021-05-30 18:27:02 -0700

    Include static files in prod dockerfile

--- cmd/api.go
+++ cmd/api.go
@@ -12,7 +12,6 @@ import (
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net/http"
-	"os"
 	"path"
 	"strings"
 
@@ -20,13 +19,7 @@ import (
 	"google.golang.org/grpc"
 )
 
-var workingDirectory string
-
 func main() {
-	dir, err := os.Getwd(); if err != nil {
-		log.Fatal(err)
-	}
-	workingDirectory = dir
 	server := grpc.NewServer(buildServerOpts()...)
 	reflection.Register(server)
 
@@ -75,7 +68,7 @@ func openapiFileHandler() http.Handler {
 
 		log.Printf("Serving %s", r.URL.Path)
 		p := strings.TrimPrefix(r.URL.Path, "/openapiv2/")
-		p = path.Join(workingDirectory, "gen/openapiv2/", p)
+		p = path.Join("gen/openapiv2/", p)
 		http.ServeFile(w, r, p)
 	})
 }
@@ -92,7 +85,7 @@ func swaggerUIHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Serving %s", r.URL.Path)
 		p := strings.TrimPrefix(r.URL.Path, "/swagger-ui/")
-		p = path.Join(workingDirectory, "swagger-ui/", p)
+		p = path.Join("swagger-ui/", p)
 		http.ServeFile(w, r, p)
 	})
 }
